6bf48156b2f3f09e43d7e8c52af14c39ad5616b3
juliussim11 <simelis.julius@gmail.com>
2022-04-25 00:44:19 +0300

    Added Link to Topbar image

--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import "./TopBar.scss";
-// import { ReactComponent as Logo } from "assets/header-logo.svg";
 import Logo from "../../assets/header-logo.svg";
 import MenuIcon from "../../assets/menu_icon.svg";
 import Menu from "../Menu/Menu";
+import { Link } from "react-router-dom";
 
 const TopBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -16,7 +16,9 @@ const TopBar = () => {
     <header className="topbar">
       <div className="topbar__content">
         <div className="topbar__content__invisible-item" />
-        <img className="topbar__content__logo" src={Logo} />
+        <Link to={`/`}>
+          <img className="topbar__content__logo" src={Logo} />
+        </Link>
         <div onClick={handleMenuClick}>
           <img className="topbar__content__menu" src={MenuIcon} />
         </div>
